006772a84dd2e888790c801b57d6ad5e2d8b694c
yangbing007 <yangbinggoooooogle@gmail.com>
2023-05-15 08:34:28 +0800

    save

--- src/index.tsx
+++ src/index.tsx
@@ -23,9 +23,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-
+    <React.StrictMode>
       <App />
-
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
